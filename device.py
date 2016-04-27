625c061224083d9b7d3f055bfeafcd25bb5993eb
59psi <brandon@brandonbianchi.com>
2016-04-27 16:48:00 -0700

    Trying to move token to header

--- device.py
+++ device.py
@@ -7,11 +7,13 @@ with open('devices.csv', "rb") as devices_source:
     reader = csv.reader(devices_source)
     devices = [row for row in reader]
 
-data = {"X-CH-Auth-Email": "brandon@brandonbianchi.com",
-        "X-CH-Auth-API-Token": "6c079ba92b2a8fe5977519f6859ecf80",
-        "devices": devices}
+headers = {"X-CH-Auth-Email": "brandon@brandonbianchi.com",
+           "X-CH-Auth-API-Token": "6c079ba92b2a8fe5977519f6859ecf80",
+           "Content-Type": "application/json; charset=utf-8"}
+
+data = {"devices": devices}
 
 req = urllib2.Request('https://cloudhelix.com/api/v1/device/create')
-req.add_header('Content-Type', 'application/json; charset=utf-8')
+req.add_header = headers
 
 response = urllib2.urlopen(req, json.dumps(data))
