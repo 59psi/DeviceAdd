53ae76d7d792078383e63bc0d6f4833bfa6031ed
59psi <brandon@brandonbianchi.com>
2016-04-27 17:24:17 -0700

    Maybe

--- device.py
+++ device.py
@@ -1,19 +1,16 @@
 import csv
 import json
-import urllib2
-
+import requests
 
 with open('devices.csv', "rb") as devices_source:
     reader = csv.reader(devices_source)
     devices = [row for row in reader]
 
+url = 'https://cloudhelix.com/api/v1/device/create'
+payload = devices
 headers = {"X-CH-Auth-Email": "brandon@brandonbianchi.com",
            "X-CH-Auth-API-Token": "6c079ba92b2a8fe5977519f6859ecf80",
            "Content-Type": "application/json; charset=utf-8"}
 
-data = {"devices": devices}
-
-req = urllib2.Request('https://cloudhelix.com/api/v1/device/create')
-req.add_header = headers
-
-response = urllib2.urlopen(req, json.dumps(data))
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+print response.content
